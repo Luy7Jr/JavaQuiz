787cf9cb0865d39bc374cc26338435a657fb0533
Luy10Jr <luy10jr3@gmail.com>
2021-05-22 16:30:40 -0400

    added new variables

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var highscore = document.querySelector(".score");
+var highscore = document.querySelector(".Hscore");
 var timeEl = document.querySelector(".timer");
-var startButton = document.querySelector(".start-button");
+var startBtn = document.querySelector(".start-button");
 var answer = document.querySelector(".answer");
 var question = document.querySelector(".question");
-
+var submitBtn = document.querySelector(".submit");
+var score = document.querySelector(".score");
 
 var timer;
 var chosenAns = "";
+var highscoreCounter;
+var correct;
+var incorrect;
+
+
+
 
