e43fee67ab8b7dea13afdd67880394a8d07b9e11
Luy10Jr <luy10jr3@gmail.com>
2021-05-22 17:55:43 -0400

    added some functions

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var question = document.querySelector(".question");
 var submitBtn = document.querySelector(".submit");
 var score = document.querySelector(".score");
 
+var score = 0:
 var timer = 60;
 var chosenAns = "";
 var highscoreCounter;
@@ -24,6 +25,45 @@ var interval = setInterval(function(){
   }
 }, 1000);
 
-
+var questions = [
+    ["Javascripts file ends in?", ".js"],
+    ["What is a Javascript element?", "Array."],
+    ["What is a string?", "element within parenthesis."],
+    ["What is a boolean?", "True or False."],
+    ["What is a loop used for?", "Used to perform a action multiple times."]
+  ];
+  
+  var cA = [];
+  var incorrect = [];
+  
+  function askQuestion(question, index) {
+    var answer = prompt(question[0], '');
+    if (answer == question[1]) {
+      alert('Correct!');
+      score++;
+      cA = question;
+    } else {
+      incorrect.push(index);
+      alert('Sorry. The correct answer is ' + question[1]);
+    }
+  }
+  
+  for (var i = 0; i < questions.length; i++) {
+    askQuestion(questions[i], i);
+  }
+  
+  var message = 'You got ' + score;
+  message += ' out of ' + questions.length;
+  message += ' questions correct.<br>' + cA;
+  
+  message += '<br><br>You answered ' + incorrect.length;
+  message += ' questions incorrect. These are as follows:';
+  
+  for(var i in incorrect){
+    message += '<p>Q ' + (incorrect[i] + 1) + '. ' + questions[incorrect[i]][0] + '<br>' + questions[incorrect[i]][1] + ' </p>';
+  }
+  
+  document.write('<p>' + message + '</p>');
+  
 
 
