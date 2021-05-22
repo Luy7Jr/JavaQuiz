cfae642aef707649add69a0fb465cc8c55ef0253
Luy10Jr <luy10jr3@gmail.com>
2021-05-22 16:54:20 -0400

    added timer

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ var question = document.querySelector(".question");
 var submitBtn = document.querySelector(".submit");
 var score = document.querySelector(".score");
 
-var timer;
+var timer = 60;
 var chosenAns = "";
 var highscoreCounter;
-var correct;
-var incorrect;
+
+function init(); {
+    getHighscore();
+}
+
+var interval = setInterval(function(){
+  document.getElementById('timer').innerHTML=count;
+  count--;
+  if (timer === 0){
+    clearInterval(interval);
+    document.getElementById('timer').innerHTML='Done';
+    alert("You're out of time!");
+  }
+}, 1000);
 
 
 
