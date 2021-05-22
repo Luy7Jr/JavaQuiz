6dadbba68233ae24cca15f590ea007f176e15c34
Luy10Jr <luy10jr3@gmail.com>
2021-05-22 16:23:18 -0400

    added more var-2

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var highscore = document.querySelector(".score");
 var timeEl = document.querySelector(".timer");
 var startButton = document.querySelector(".start-button");
 var answer = document.querySelector(".answer");
+var question = document.querySelector(".question");
+
 
 var timer;
-
+var chosenAns = "";
 
