4b6ee124609248f9f9c77895f6404b1787978ca1
Luy10Jr <luy10jr3@gmail.com>
2021-05-23 13:34:04 -0400

    added question variable

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var timeEl = document.querySelector(".time");
+var secondsLeft = 60;
+var scoreEl = document.querySelector("#score");
+
+var introEl = document.querySelector("#intro");
+
+var questionEl = document.querySelector("#questions");
+
